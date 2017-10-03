bf90b129cea9904a7152969d51edcb114ebfefe2
ANTONIO <ANCLIST@GMAIL.COM>
2017-10-03 16:45:28 -0400

    Add portfolio route

--- server.rb
+++ server.rb
@@ -4,3 +4,8 @@ get '/home' do
   # instructions for what to do when user visits '/home' will go here
   erb :index
 end
+
+get '/portfolio' do
+  # instructions for what to do when user visits '/home' will go here
+  erb :gallery
+end
